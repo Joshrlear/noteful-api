63c29de7512778dd7f578f2f4c311d7c41b9b9ab
Josh Lear <joshrlear@gmail.com>
2019-07-19 09:41:52 -0700

    connect to frontend

---
+++ src/notes/notes-service.js
@@ -0,0 +1,41 @@
+const NotesService = {
+    getAllNotes(knex) {
+        return knex
+            .select('*')
+            .from('noteful_notes')
+    },
+
+    insertNote(knex, newNotes) {
+        return knex
+            .insert(newNotes)
+            .into('noteful_notes')
+            .returning('*')
+            .then(rows => {
+                return rows[0]
+            })
+    },
+
+    getById(knex, id) {
+        return knex
+            .from('noteful_notes')
+            .select('*')
+            .where({ id })
+            .first()
+    },
+
+    deleteNote(knex, id) {
+        return knex
+            .from('noteful_notes')
+            .where({ id })
+            .delete()
+    },
+
+    updateNote(knex, id, newNotesFields) {
+        return knex
+            .from('noteful_notes')
+            .where({ id })
+            .update(newNotesFields)
+    },
+}
+
+module.exports = NotesService
